0772c048ceaf33057995a176aef5298bb852536f
KunFay99 <kaffayukiffu@gmail.com>
2024-10-30 14:06:45 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+print("""[0] pip\n[1] pip3\nWhich one do you use?""")
+
+c = input(">>>: ")
+if c == "0":
+    os.system("apt install seanime")
+    os.system("apt install serd")
+    os.system("apt install strace")
+    os.system("ape install util-linux")
+    os.system("aptninstall python")
+    os.system("apt install x11-repo")
+    os.system("apt install php")
+    os.system("pip install aiohtt")
+    os.system("PIP install service apache2 star")
+    os.system("PIP install service apache2 status")
+elif c == "1":
+    os.system("apt install seanime")
+    os.system("apt install serd")
+    os.system("apt install strace")
+    os.system("ape install util-linux")
+    os.system("apt install python")
+    os.system("apt install x11-repo")
+    os.system("apt install php")
+    os.system("pip install aiohtt")
+    os.system("PIP install service apache2 star")
+    os.system("PIP install service apache2 status")
+print("Done.")
+
+
+
+
+
+
+git clone https://github.com/KunFay99/Kunkaffa24.git
