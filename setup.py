1dfd4814801edc37eab0aafc3fc74f95c2b070a0
KunFay99 <kaffayukiffu@gmail.com>
2024-11-01 14:15:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,14 @@ if c == "0":
     os.system("apt-get install serd")
     os.system("apt-install strace")
     os.system("pip install slowloris")
+    os.system("pip3 install colorama")
     os.system("git pull")
 elif c == "1":
-os.system("apt-get install seanime")
+    os.system("apt-get install seanime")
     os.system("apt-get install serd")
     os.system("apt-install strace")
     os.system("pip3 install slowloris")
+    os.system("pip3 install colorama")
     os.system("git pull")
     print("Done.")
 
