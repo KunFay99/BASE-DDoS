dabe24336bd769a35bf86257e3a4f7602eef76e7
KunFay99 <kaffayukiffu@gmail.com>
2024-11-01 14:39:30 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,11 @@ print("""[0] pip\n[1] pip3\nWhich one do you use?""")
 
 c = input(">>>: ")
 if c == "0":
-    os.system("apt-get install seanime")
-    os.system("apt-get install serd")
-    os.system("apt-install strace")
     os.system("pip install slowloris")
     os.system("pip install colorama")
     os.system("pip install getpass")
     os.system("git pull")
 elif c == "1":
-    os.system("apt-get install seanime")
-    os.system("apt-get install serd")
-    os.system("apt-install strace")
     os.system("pip3 install slowloris")
     os.system("pip3 install colorama")
     os.system("pip3 install getpass")
